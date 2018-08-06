da12c04cf9ab2aee8d1f89322a583fba9a54c426
Vincent L <vincentluoster93@gmail.com>
2018-08-06 03:59:59 -0700

    update mongoose

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 
 // connection url
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri)
+mongoose.connect(config.mongoUri, { useNewUrlParser: true })
 
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`)
