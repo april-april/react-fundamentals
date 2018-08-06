f27b05b50021822d586bc671ab687fc7d06aea78
Vincent L <vincentluoster93@gmail.com>
2018-08-05 21:08:27 -0700

    unique error

--- server/helpers/dbErrorHandler.js
+++ server/helpers/dbErrorHandler.js
@@ -1,3 +1,17 @@
+'use strict'
+
+// get unique error name
+const getUniqueErrorMessage = (err) => {
+    let output
+    try {
+        let fieldName = err.message.substring(err.message.lastIndexOf('.$') + 2, err.message.lastIndexOf('_1'))
+        output = fieldName.charAt(0).toUpperCase() + fieldName.slice(1) + ' already exists'
+    } catch (ex) {
+        output = 'Unique field already exists'
+    }
+
+    return output
+}
 
 // get the error message
 const getErrorMessage = (err) => {
