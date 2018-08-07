667ccebc11bc134e81243b14beaf22ee5db28f4c
Vincent L <vincentluoster93@gmail.com>
2018-08-07 00:49:39 -0700

    delete, last of crud

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -50,3 +50,22 @@ const update = (params, credentials, user) => {
       return response.json()
     }).catch((err) => console.log(err))
 }
+
+// delete user from database
+const remove = (params, credentials) => {
+    return fetch('/api/users/' + params.userId, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+    }
+    }).then((response) => {
+      return response.json()
+    }).catch((err) => console.log(err))
+}
+
+export {
+    create, list, read, update, remove
+  }
+  
