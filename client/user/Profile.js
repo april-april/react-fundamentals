b001cafef25a65f2688144823522ad694919523c
Vincent L <vincentluoster93@gmail.com>
2018-08-08 05:37:18 -0700

    profile update

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -55,7 +55,45 @@ class Profile extends Component {
 	componentDidMount = () => {
 		this.init(this.match.params.userId)
 	}
-	
+	render() {
+		const {classes} = this.props
+		const redirectToSignin = this.state.redirectToSignin
+		if (redirectToSignin) {
+		return <Redirect to='/signin'/>
+		}
+		return (
+		<Paper className={classes.root} elevation={4}>
+			<Typography type="title" className={classes.title}>
+			Profile
+			</Typography>
+			<List dense>
+			<ListItem>
+				<ListItemAvatar>
+				<Avatar>
+					<Person/>
+				</Avatar>
+				</ListItemAvatar>
+				<ListItemText primary={this.state.user.name} secondary={this.state.user.email}/> {
+				auth.isAuthenticated().user && auth.isAuthenticated().user._id == this.state.user._id && 
+				(<ListItemSecondaryAction>
+					<Link to={"/user/edit/" + this.state.user._id}>
+					<IconButton aria-label="Edit" color="primary">
+						<Edit/>
+					</IconButton>
+					</Link>
+					<DeleteUser userId={this.state.user._id}/>
+				</ListItemSecondaryAction>)
+				}
+			</ListItem>
+			<Divider/>
+			<ListItem>
+				<ListItemText primary={"Joined: " + (
+				new Date(this.state.user.created)).toDateString()}/>
+			</ListItem>
+			</List>
+		</Paper>
+		)
+	}
 }
 
 Profile.propTypes = {
