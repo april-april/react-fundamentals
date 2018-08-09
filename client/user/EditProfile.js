9aa0b1b1ea9b60fa24eb96ec3722f0656c826f71
Vincent L <vincentluoster93@gmail.com>
2018-08-08 18:09:40 -0700

    need to style

--- client/user/EditProfile.js
+++ client/user/EditProfile.js
@@ -1,11 +1,21 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Card, {CardActions, CardContent} from 'material-ui/Card'
 import Button from 'material-ui/Button'
 import TextField from 'material-ui/TextField'
 import Typography from 'material-ui/Typography'
+import Icon from 'material-ui/Icon'
+import PropTypes from 'prop-types'
+import {withStyles} from 'material-ui/styles'
+import auth from './../auth/auth-helper'
+import {read, update} from './api-user.js'
+import {Redirect} from 'react-router-dom'
+
+
+
+// NEED TO STYLE
 
 class EditProfile extends Component {
-    constructor({match}) {
+    constructor({ match }) {
         super()
         this.state = {
             name: '',
@@ -53,8 +63,40 @@ class EditProfile extends Component {
     handleChange = name => event => {
         this.setState({[name]: event.target.value})
     }
+
+    render(){
+        const { classes } = this.props
+        if (this.state.redirectToProfile) {
+            return (<Redirect to={'/user/' + this.state.userId}/>)
+        }
+
+        return (
+            <Card className={classes.card}>
+                <CardContent>
+                    <Typography type="headline" component="h2" className={classes.title}>
+                    Edit Profile
+                    </Typography>
+                    <TextField id="name" label="Name" className={classes.textField} value={this.state.name} onChange={this.handleChange('name')} margin="normal"/><br/>
+                    <TextField id="email" type="email" label="Email" className={classes.textField} value={this.state.email} onChange={this.handleChange('email')} margin="normal"/><br/>
+                    <TextField id="password" type="password" label="Password" className={classes.textField} value={this.state.password} onChange={this.handleChange('password')} margin="normal"/>
+                    <br/> {
+                        this.state.error && (<Typography component="p" color="error">
+                            <Icon color="error" className={classes.error}>error</Icon>
+                            {this.state.error}
+                        </Typography>)
+                    }
+                </CardContent>
+                <CardActions>
+                    <Button color="primary" variant="raised" onClick={this.clickSubmit} className={classes.submit}>Submit</Button>
+                </CardActions>
+            </Card>
+        )
+
+    }
 }
 
 EditProfile.propTypes = {
     classes: PropTypes.object.isRequired
 }
+
+export default withStyles(styles)(EditProfile)
