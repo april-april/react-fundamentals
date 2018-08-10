f1229b3463dace49f52e3c2f2edb9b482c89a3e1
Vincent L <vincentluoster93@gmail.com>
2018-08-09 22:33:28 -0700

    menu

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -5,6 +5,7 @@ import Typography from 'material-ui/Typography'
 import IconButton from 'material-ui/IconButton'
 import HomeIcon from 'material-ui-icons/Home'
 import Button from 'material-ui/Button'
+import auth from './../auth/auth-helper'
 import { Link, withRouter } from 'react-router-dom'
 
 const isActive = (history, path) => {
@@ -28,6 +29,30 @@ const Menu = withRouter(() => (
             <Link to="/users">
                 <Button style={isActive(history, "/users")}>Users</Button>
             </Link>
+            {
+                !auth.isAuthenticated() && 
+                (<span>
+                  <Link to="/signup">
+                    <Button style={isActive(history, "/signup")}>Sign up
+                    </Button>
+                  </Link>
+                  <Link to="/signin">
+                    <Button style={isActive(history, "/signin")}>Sign In
+                    </Button>
+                  </Link>
+                </span>)   
+            }
+            {
+                auth.isAuthenticated() && (
+                    <span>
+                      <Link to={"/user/" + auth.isAuthenticated().user._id}>
+                        <Button style={isActive(history, "/user/" + auth.isAuthenticated().user._id)}>My Profile</Button>
+                      </Link>
+                      <Button color="inherit" onClick={() => {
+                          auth.signout(() => history.push('/'))
+                        }}>Sign out</Button>
+                    </span>)
+            }
         </Toolbar>
     </AppBar>
 ))
