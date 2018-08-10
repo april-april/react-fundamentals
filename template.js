663c2175cab52c9b96fdbd86e5737d69a562b85b
Vincent L <vincentluoster93@gmail.com>
2018-08-09 23:38:38 -0700

    main.js update

--- template.js
+++ template.js
@@ -1,15 +1,22 @@
-export default () => {
-    return `<!doctype html>
-      <html lang="en">
-        <head>
-          <meta charset="utf-8">
-          <title>Hello World</title>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-        </head>
-        <body>
-          <div id="root">Hola world</div>
-          <script type="text/javascript" src="/dist/bundle.js"></script>
-        </body>
-      </html>`
+export default ({markup, css}) => 
+{
+  return `<!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Play that audio</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <style>
+            a{
+              text-decoration: none
+            }
+        </style>
+      </head>
+      <body style="margin:0">
+        <div id="root">${markup}</div>
+        <style id="jss-server-side">${css}</style>
+        <script type="text/javascript" src="/dist/bundle.js"></script>
+      </body>
+    </html>`
 }
