c1ccf83ee9728b1b15036d2959cc852d4d57a82c
Vincent L <vincentluoster93@gmail.com>
2018-08-09 23:27:15 -0700

    css theme random

--- server/express.js
+++ server/express.js
@@ -51,6 +51,27 @@ app.get('/', (req, res) => {
 
 app.get('*', (req, res) => {
     const sheetsRegistry = new SheetsRegistry()
+    const generateClassName = createGenerateClassName()
+    const generateClassName = createGenerateClassName()
+    const theme = createMuiTheme({
+        palette: {
+            primary: {
+            light: '#757de8',
+            main: '#3f51b5',
+            dark: '#002984',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ff79b0',
+            main: '#ff4081',
+            dark: '#c60055',
+            contrastText: '#000',
+        },
+            openTitle: indigo['400'],
+            protectedTitle: pink['400'],
+            type: 'light'
+        },
+    })
 })
 
 // Catch unauthorised errors
